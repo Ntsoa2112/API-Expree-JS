91ea57f0a0e2ec1c055edf1495959a8b41922af7
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2023-03-30 17:26:09 +0300

    initial commit

---
+++ service/service.js
@@ -0,0 +1,61 @@
+const jwt = require("jsonwebtoken");
+const SECRET = "mykey";
+const fs = require("fs");
+
+module.exports = {
+    generer_token: function (id, email) {
+        const token = jwt.sign(
+            {
+                id: id,
+                email: email,
+            },
+            SECRET,
+            { 
+                expiresIn: "2d" 
+            }
+        );
+        return token;
+    },
+
+    uploadFile: function (chemin, fichier, add_name) {
+        return new Promise((resolve, reject) => {
+            let uploadPath, current_time = new Date().getTime(), nom_img;
+    
+            if (!fichier) {
+              reject("No files were uploaded.");
+            }
+        
+            let fichier_name = fichier.name.split(".");
+            let ext = fichier_name[fichier_name.length - 1];
+            nom_img = add_name + "_iTeams-s_" + current_time + "." + ext;
+            uploadPath = chemin + nom_img;
+        
+            fichier.mv(uploadPath, function (err) {
+              if (err) reject(err);
+              resolve(uploadPath)
+            });
+        })
+      },
+    
+      deleteFile: function (path) {
+        if (path) {
+          try {
+            fs.unlink(path, (err => {
+                if (err) throw err;
+                else {
+                    return true;
+                }
+              }));            
+          } catch (err) {
+            throw err;
+          }
+        } else {
+          throw "Aucun chemin";
+        }
+      },
+
+      formatDate: function(date){
+        let y = date.getFullYear(), m = date.getMonth() +1 , d = date.getDate(), h = date.getHours(), min = date.getMinutes(), sec = date.getSeconds();
+        return y+'-'+m+'-'+d+' '+h+':'+min+':'+sec;
+    }
+}
